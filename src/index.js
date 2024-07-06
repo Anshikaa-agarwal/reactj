b582fdf0e355a782bf3dd2f38b554d61eb573cfa
Anshika <anshika2020ag@gmail.com>
2024-07-06 21:12:03 +0530

    netflix picks

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<>
+  <h1>NETFLIX</h1>
+  <p>Here's a list of netflix movies: </p>
+  <ol>
+    <li>movie1</li>
+    <li>movie2</li>
+    <li>movie3</li>
+    <li>movie4</li>
+    <li>movie5</li>
+  </ol>
+</>)
