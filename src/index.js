ce3c3aff29ed95097f9098fc9536ccd9e62fc077
Anshika <anshika2020ag@gmail.com>
2024-07-06 23:44:22 +0530

    date

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+const d = new Date();
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<>
-  <h1>NETFLIX</h1>
-  <p>Here's a list of netflix movies: </p>
-  <ol>
-    <li>movie1</li>
-    <li>movie2</li>
-    <li>movie3</li>
-    <li>movie4</li>
-    <li>movie5</li>
-  </ol>
+  <h1>Date and Time</h1>
+  <p>Date: {d.getDate}/{d.getMonth}/{d.getFullYear}</p>
 </>)
