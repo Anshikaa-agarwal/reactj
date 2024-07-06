159b319fd0c6febb2c33a8e480764799e1eb2160
Anshika <anshika2020ag@gmail.com>
2024-07-06 23:48:16 +0530

    time

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 const d = new Date();
-
+const t = new Date().toLocaleTimeString();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<>
   <h1>Date and Time</h1>
-  <p>Date: {d.getDate()}/{d.getMonth()}/{d.getFullYear()}</p>
+  <p>Date: {d.getDate()}/{d.getMonth()+1}/{d.getFullYear()}</p>
+  <p>Time: {t} </p>
 </>)
